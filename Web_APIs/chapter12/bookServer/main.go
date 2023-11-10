9bc44eb3e883f5c5bf14c2d6179ce2fe7aa029e3
Otero <oteronickbrian@gmail.com>
2023-11-10 23:49:19 +0300

    REST services for deployement

---
+++ Web_APIs/chapter12/bookServer/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Book hods data of a book
+type Book struct {
+	ID 	int
+	ISBN string
+	Author string
+	publishedYear string
+}
+
+func main() {
+	// File open for reading, writing and appending
+	f, err := os.OpenFile("app.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+	}
+	defer f.Close()
+	// This attaches program logs to file
+	log.SetOutput(f)
+	
+}
