99f1701882c87bdbb7a25388aaba7e2fee8e857e
Otero <oteronickbrian@gmail.com>
2023-11-20 23:51:59 +0300

    mongodb and Go

---
+++ Web_APIs/chapter5/intro/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// Movie holds a movie data
+type Movie struct {
+	Name string `bson:"name"`
+	Year string `bson:year`
+	Directors []string `bson:"directors"`
+	Writers []string `bson:"writers"`
+	BoxOffice `bson:"boxOffice"`
+}
+
+// BoxOffice is nested in Movie
+type BoxOffice struct {
+	Budget unint64 `bson:"budget"`
+	Gross unint64 `bson:gross`
+}
